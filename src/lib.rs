7126ba0f2759614acda38e230bd472ab72f4c22d
Matthew <matthew.boman@gmail.com>
2023-01-18 14:23:33 -0500

    buffers and indices

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use wgpu::util::DeviceExt;
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
@@ -7,6 +8,48 @@ use winit::{
 #[cfg(target_arch="wasm32")]
 use wasm_bindgen::prelude::*;
 
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color:    [f32; 3],
+}
+
+impl Vertex {
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress, // 24 bytes
+            step_mode:    wgpu::VertexStepMode::Vertex,
+            attributes:   &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3 // vec3<f32>
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x3
+                }
+            ]
+        }
+    }
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] },
+];
+
+const INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
+];
+
 struct State {
     surface:         wgpu::Surface,
     device:          wgpu::Device,
@@ -15,6 +58,10 @@ struct State {
     size:            winit::dpi::PhysicalSize<u32>,
     window:          Window,
     render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer:   wgpu::Buffer,
+    num_vertices:    u32,
+    index_buffer:    wgpu::Buffer,
+    num_indices:     u32,
 }
 
 impl State {
@@ -89,7 +136,9 @@ impl State {
             vertex:   wgpu::VertexState {
                 module:      &shader,
                 entry_point: "vs_main",
-                buffers:     &[],
+                buffers:     &[
+                    Vertex::desc(),
+                ],
             },
             fragment: Some(wgpu::FragmentState {
                 module:      &shader,
@@ -118,6 +167,25 @@ impl State {
             multiview: None,
          });
 
+         let vertex_buffer = device.create_buffer_init(
+             &wgpu::util::BufferInitDescriptor {
+                 label:    Some("Vertex Buffer"),
+                 contents: bytemuck::cast_slice(VERTICES),
+                 usage:    wgpu::BufferUsages::VERTEX,
+             }
+         );
+
+         let index_buffer = device.create_buffer_init(
+             &wgpu::util::BufferInitDescriptor {
+                 label:    Some("Index Buffer"),
+                 contents: bytemuck::cast_slice(INDICES),
+                 usage:    wgpu::BufferUsages::INDEX,
+             }
+         );
+
+         let num_vertices = VERTICES.len() as u32;
+         let num_indices  = INDICES.len() as u32;
+
          surface.configure(&device, &config);
 
          Self {
@@ -128,6 +196,10 @@ impl State {
              config,
              size,
              render_pipeline,
+             vertex_buffer,
+             num_vertices,
+             index_buffer,
+             num_indices,
          }
     }
 
@@ -183,7 +255,9 @@ impl State {
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
-            render_pass.draw(0..3, 0..1);
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
         self.queue.submit(std::iter::once(encoder.finish()));
