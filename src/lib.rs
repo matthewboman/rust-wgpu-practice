65c5bb3d01c7bf2934912f2a8fbab4505d81cf01
Matthew <matthew.boman@gmail.com>
2023-01-20 10:41:35 -0500

    textures and bind groups

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::iter;
+
 use wgpu::util::DeviceExt;
 use winit::{
     event::*,
@@ -8,15 +10,19 @@ use winit::{
 #[cfg(target_arch="wasm32")]
 use wasm_bindgen::prelude::*;
 
+mod texture;
+
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 struct Vertex {
-    position: [f32; 3],
-    color:    [f32; 3],
+    position:   [f32; 3],
+    tex_coords: [f32; 2],
 }
 
 impl Vertex {
     fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        use std::mem;
+
         wgpu::VertexBufferLayout {
             array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress, // 24 bytes
             step_mode:    wgpu::VertexStepMode::Vertex,
@@ -29,7 +35,7 @@ impl Vertex {
                 wgpu::VertexAttribute {
                     offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
                     shader_location: 1,
-                    format: wgpu::VertexFormat::Float32x3
+                    format: wgpu::VertexFormat::Float32x2
                 }
             ]
         }
@@ -37,12 +43,13 @@ impl Vertex {
 }
 
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] },
-    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] },
-    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] },
-    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] },
-    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] },
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], tex_coords: [0.4131759, 0.00759614], }, // A
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], tex_coords: [0.0048659444, 0.43041354], }, // B
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], tex_coords: [0.28081453, 0.949397], }, // C
+    Vertex { position: [0.35966998, -0.3473291, 0.0], tex_coords: [0.85967, 0.84732914], }, // D
+    Vertex { position: [0.44147372, 0.2347359, 0.0], tex_coords: [0.9414737, 0.2652641], }, // E
 ];
+
 
 const INDICES: &[u16] = &[
     0, 1, 4,
@@ -51,17 +58,21 @@ const INDICES: &[u16] = &[
 ];
 
 struct State {
-    surface:         wgpu::Surface,
-    device:          wgpu::Device,
-    queue:           wgpu::Queue,
-    config:          wgpu::SurfaceConfiguration,
-    size:            winit::dpi::PhysicalSize<u32>,
-    window:          Window,
-    render_pipeline: wgpu::RenderPipeline,
-    vertex_buffer:   wgpu::Buffer,
-    num_vertices:    u32,
-    index_buffer:    wgpu::Buffer,
-    num_indices:     u32,
+    surface:            wgpu::Surface,
+    device:             wgpu::Device,
+    queue:              wgpu::Queue,
+    config:             wgpu::SurfaceConfiguration,
+    size:               winit::dpi::PhysicalSize<u32>,
+    render_pipeline:    wgpu::RenderPipeline,
+    vertex_buffer:      wgpu::Buffer,
+    num_vertices:       u32,
+    index_buffer:       wgpu::Buffer,
+    num_indices:        u32,
+    #[allow(dead_code)]
+    diffuse_bind_group: wgpu::BindGroup,
+    diffuse_texture:    texture::Texture,
+    window:             Window,
+
 }
 
 impl State {
@@ -96,41 +107,91 @@ impl State {
             .unwrap();
          */
 
-         let (device, queue) = adapter.request_device(
-             &wgpu::DeviceDescriptor {
-                 features: wgpu::Features::empty(),
-                 // WebGL doesn't support all wgpu's features, so disable some if building for web.
-                 limits:   if cfg!(target_arch = "wasm32") {
-                     wgpu::Limits::downlevel_webgl2_defaults()
-                 } else {
-                     wgpu::Limits::default()
-                 },
-                 label:    None,
-             },
-             None, // trace path
-         ).await.unwrap();
+        let (device, queue) = adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                features: wgpu::Features::empty(),
+                // WebGL doesn't support all wgpu's features, so disable some if building for web.
+                limits:   if cfg!(target_arch = "wasm32") {
+                    wgpu::Limits::downlevel_webgl2_defaults()
+                } else {
+                    wgpu::Limits::default()
+                },
+                label:    None,
+            },
+            None, // trace path
+        ).await.unwrap();
 
-         let config = wgpu::SurfaceConfiguration {
-             usage:        wgpu::TextureUsages::RENDER_ATTACHMENT,
-             format:       surface.get_supported_formats(&adapter)[0], // the prefered format is placed at the beginning of the vector
-             width:        size.width,
-             height:       size.height,
-             present_mode: wgpu::PresentMode::Fifo, // VSync, likely supported on all platforms
-             alpha_mode:   wgpu::CompositeAlphaMode::Auto,
-         };
+        let config = wgpu::SurfaceConfiguration {
+            usage:        wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format:       surface.get_supported_formats(&adapter)[0], // the prefered format is placed at the beginning of the vector
+            width:        size.width,
+            height:       size.height,
+            present_mode: wgpu::PresentMode::Fifo, // VSync, likely supported on all platforms
+            alpha_mode:   wgpu::CompositeAlphaMode::Auto,
+        };
 
-         let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-             label:  Some("Shader"),
-             source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
-         });
+        surface.configure(&device, &config);
 
-         let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        let diffuse_bytes   = include_bytes!("maple.png");
+        let diffuse_texture = texture::Texture::from_bytes(
+            &device,
+            &queue,
+            diffuse_bytes,
+            "maple.png"
+        ).unwrap();
+
+        let texture_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding:    0,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty:         wgpu::BindingType::Texture {
+                        multisampled:   false,
+                        view_dimension: wgpu::TextureViewDimension::D2,
+                        sample_type:    wgpu::TextureSampleType::Float { filterable: true },
+                    },
+                    count:      None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding:    1,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    // This should match the filterable field of the corresponding Texture entry about
+                    ty:         wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                    count:      None,
+                },
+            ],
+            label: Some("texture_bind_group_layout"),
+        });
+
+        let diffuse_bind_group = device.create_bind_group(
+                &wgpu::BindGroupDescriptor {
+                    layout:  &texture_bind_group_layout,
+                    entries: &[
+                        wgpu::BindGroupEntry {
+                            binding:  0,
+                            resource: wgpu::BindingResource::TextureView(&diffuse_texture.view)
+                        },
+                        wgpu::BindGroupEntry {
+                            binding:  1,
+                            resource: wgpu::BindingResource::Sampler(&diffuse_texture.sampler)
+                        },
+                    ],
+                    label:   Some("diffuse_bind_group")
+                }
+        );
+
+        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label:  Some("Shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        });
+
+        let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
             label: Some("Render Pipeline Layout"),
-            bind_group_layouts:   &[],
+            bind_group_layouts:   &[&texture_bind_group_layout],
             push_constant_ranges: &[],
-         });
+        });
 
-         let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label:    Some("Render Pipeline"),
             layout:   Some(&render_pipeline_layout),
             vertex:   wgpu::VertexState {
@@ -165,42 +226,42 @@ impl State {
                 alpha_to_coverage_enabled: false
             },
             multiview: None,
-         });
+        });
 
-         let vertex_buffer = device.create_buffer_init(
-             &wgpu::util::BufferInitDescriptor {
-                 label:    Some("Vertex Buffer"),
-                 contents: bytemuck::cast_slice(VERTICES),
-                 usage:    wgpu::BufferUsages::VERTEX,
-             }
-         );
+        let vertex_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label:    Some("Vertex Buffer"),
+                contents: bytemuck::cast_slice(VERTICES),
+                usage:    wgpu::BufferUsages::VERTEX,
+            }
+        );
 
-         let index_buffer = device.create_buffer_init(
-             &wgpu::util::BufferInitDescriptor {
-                 label:    Some("Index Buffer"),
-                 contents: bytemuck::cast_slice(INDICES),
-                 usage:    wgpu::BufferUsages::INDEX,
-             }
-         );
+        let index_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label:    Some("Index Buffer"),
+                contents: bytemuck::cast_slice(INDICES),
+                usage:    wgpu::BufferUsages::INDEX,
+            }
+        );
 
-         let num_vertices = VERTICES.len() as u32;
-         let num_indices  = INDICES.len() as u32;
+        let num_vertices = VERTICES.len() as u32;
+        let num_indices  = INDICES.len() as u32;
 
-         surface.configure(&device, &config);
-
-         Self {
-             window,
-             surface,
-             device,
-             queue,
-             config,
-             size,
-             render_pipeline,
-             vertex_buffer,
-             num_vertices,
-             index_buffer,
-             num_indices,
-         }
+        Self {
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            render_pipeline,
+            vertex_buffer,
+            num_vertices,
+            index_buffer,
+            num_indices,
+            diffuse_bind_group,
+            diffuse_texture,
+        }
     }
 
     pub fn window(&self) -> &Window {
@@ -255,6 +316,7 @@ impl State {
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
             render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
             render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
